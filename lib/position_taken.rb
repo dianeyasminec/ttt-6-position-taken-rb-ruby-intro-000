fd44dd8fb73b415ebd23f8102a2e33c7fd2949f7
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-20 22:50:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,12 @@
 def position_taken? (board=[],index=0)
   if board[index]== " " || board[index]== "" || board[index]== nil
     return false
-  else 
+  else
     if board[index]=="X" || board[index]=="O"
       return true
     end
   end
-  
+
 end
 board=[1,"",2,4]
 position_taken?
